e18f3b76126e9ff427aba76a999b4be879bb8802
unknown <anhtuphamxt@gamil.com>
2021-06-15 22:19:25 +0700

    1506

--- 1506_[TH]Customer-Management/src/main/java/com/codegym/controller/CustomerController.java
+++ 1506_[TH]Customer-Management/src/main/java/com/codegym/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,11 +22,13 @@ public class CustomerController {
         List<Customer> customers = customerService.findAll();
         ModelAndView modelAndView = new ModelAndView("/customer/list");
         modelAndView.addObject("customers", customers);
+        modelAndView.addObject("message","");
+
         return modelAndView;
     }
 
     @GetMapping("/customer/create")
-    public ModelAndView addNew() {
+    public ModelAndView addNewForm() {
         ModelAndView modelAndView = new ModelAndView("/customer/create");
         modelAndView.addObject("customer", new Customer());
         return modelAndView;
@@ -36,6 +39,39 @@ public class CustomerController {
         ModelAndView modelAndView = new ModelAndView("/customer/create");
         customerService.createCustomer(customer);
         modelAndView.addObject("message","tao thanh cong");
+        modelAndView.addObject("customer",new Customer());
+        return modelAndView;
+    }
+    @GetMapping("/customer/{id}/edit")
+    public  ModelAndView editForm(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("customer",customer);
+        modelAndView.addObject("message","");
+        return modelAndView;
+    }
+    @PostMapping("/customer/update")
+    public ModelAndView update(@ModelAttribute Customer customer) {
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        customerService.updateCustomer(customer.getId(),customer);
+        modelAndView.addObject("message","sua thanh cong");
+        modelAndView.addObject("customer",customer);
+        return modelAndView;
+    }
+    @GetMapping("/customer/{id}/delete")
+    public  ModelAndView delete(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        customerService.deleteCustomer(id);
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers",customers);
+        modelAndView.addObject("message","xoa thanh cong");
+        return modelAndView;
+    }
+    @GetMapping("/customer/{id}/view")
+    public  ModelAndView viewForm(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("/customer/view");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("customer",customer);
         return modelAndView;
     }
 }
