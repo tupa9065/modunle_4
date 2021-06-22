c2cefc1d5112b8c9894c5dd9dead01a8db5c942a
unknown <anhtuphamxt@gamil.com>
2021-06-22 16:45:53 +0700

    2206

---
+++ 2206_[TH]validate-infor-user/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,42 @@
+package com.codegym.model;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class User {
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    String name;
+
+    @Min(16)
+    @Max(35)
+    int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
